ce56c351d11586c20099eb9005f95ac111de859d
Dirma-dayo <fahribaadila3@gmail.com>
2025-09-06 13:25:22 +0700

    miaw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,14 @@
-import "./globals.css";
-import Header from "../component/header";
-import Footer from "../component/footer";
+import { ReactNode } from "react";
 
-export const metadata = {
-  title: "school collab site",
-  description: "made for fun",
-};
+interface RootLayoutProps {
+  children: ReactNode;
+}
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-1 p-4">{children}</main>
-        <Footer />
+        {children}
       </body>
     </html>
   );
